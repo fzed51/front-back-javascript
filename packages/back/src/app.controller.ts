886cff01bafae9a331bbdcbd332e3317ae2e7a2d
Fabien Sanchez <fzed51+git@gmail.com>
2024-09-07 22:15:08 +0200

    add 1st route

--- packages/back/src/app.controller.ts
+++ packages/back/src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { version } from '../package.json';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('ping')
+  ping() {
+    return {
+      version: version,
+      now: new Date().toJSON(),
+    };
   }
 }
