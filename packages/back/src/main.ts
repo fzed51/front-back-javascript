0b54f57ca81cf982b62f5dbb49d1e5bcfb05ce6e
Fabien Sanchez <fzed51+git@gmail.com>
2024-09-07 22:15:22 +0200

    enable cors

--- packages/back/src/main.ts
+++ packages/back/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: ['hrrp://localhost:5173'] });
   await app.listen(3000);
 }
 bootstrap();
